3f2d5c9a19c44818503e5d293996f716d438b117
vinicius <viniciuslemoscontato21@gmail.com>
2020-08-19 00:02:15 -0300

    deu certo a migrations e os relacionamentos

--- src/database/migrations/1597798570770-CreateUsers.ts
+++ src/database/migrations/1597798570770-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1595376549704 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
@@ -19,11 +19,11 @@ export default class CreateUsers1595376549704 implements MigrationInterface {
           },
           {
             name: 'email',
-            type: 'string',
+            type: 'varchar',
             isUnique: false,
           },
           {
-            name: 'Password',
+            name: 'password',
             type: 'varchar',
           },
           {
